db10b7f5ca844e7a68c4cad5698f54f73cd46691
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2026-01-12 14:03:33 +0700

    Refactor app structure and remove unused files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+// data list
+const listItems = [
+  { id: 1, title: "Eat", done: false },
+  { id: 2, title: "Drink", done: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <CheckList />
+      <Stats />
     </div>
+  );
+}
+
+// components logo
+
+function Logo() {
+  return <span className="logo">✅ GoCheck 📝</span>;
+}
+
+// components form
+function Form() {
+  const [title, setTitle] = useState("");
+
+  function handleChange(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Ada yang mau kamu catat ?</h3>
+      <input
+        type="text"
+        name="title"
+        id=""
+        onChange={handleChange}
+        value={title}
+      />
+      <button>➕</button>
+    </form>
+  );
+}
+
+// components CheckList
+function CheckList() {
+  return (
+    <div className="list">
+      <ul>
+        {listItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// function Item dengan membuat props
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span style={{ textDecoration: item.done ? "line-through" : "" }}>
+        {item.title}
+      </span>
+      <button>🗑</button>
+    </li>
+  );
+}
+
+// components Stats
+function Stats() {
+  return (
+    <footer className="stats">
+      <span>✅ Kamu punya x catatan dan baru x yang di checklist (x%) 📝</span>
+    </footer>
   );
 }
 
